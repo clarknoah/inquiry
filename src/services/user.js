32dcb68b66b31b03a6907cb3ad412f77bef209cd
Noah Clark <noahbc08@gmail.com>
2022-08-28 19:37:20 +0200

    fixed null

--- src/services/user.js
+++ src/services/user.js
@@ -14,7 +14,7 @@ let user = {
 
     },
     getUser:()=>{
-        if(localStorage.activeUser_json!==null){
+        if(localStorage.activeUser_json!=null){
             let props = JSON.parse(localStorage.activeUser_json);
             let activeUser = InquiryModel.getExistingModelClass("User",parseInt(localStorage.activeUser_id), props);
             return activeUser;
